3578e35cb144552cf29c735028ed81b042dcd08c
Nandor Devai <mail@devainandor.com>
2019-04-16 17:30:15 +0200

    reformat, handle keyboard interrupt

--- tepoti.py
+++ tepoti.py
@@ -1,32 +1,35 @@
 import time
 import math
 import os
+import sys
 
 def notify(title, text):
     os.system("""
-              osascript -e 'display notification "{}" with title "{}"'
-              """.format(text, title))
-
+osascript -e 'display notification "{}" with title "{}"'
+""".format(text, title))
+    
 def getTime(seconds):
-	return (math.floor(seconds / 60), seconds % 60)
-
+    return (math.floor(seconds / 60), seconds % 60)
+    
 def formatTime(min, sec):
-	return str(min).rjust(2, '0') + ':' + str(sec).rjust(2, '0')
+    return str(min).rjust(2, '0') + ':' + str(sec).rjust(2, '0')
 
 def show_pomodoro():
-	show_timer('Pomodoro', 1200)
+    show_timer('Pomodoro', 1500)
 
 def show_break():
-	show_timer('Break', 300)
+    show_timer('Break', 300)
 
 def show_timer(msg, seconds):
-	for i in range(0, seconds):
-		print(msg, formatTime(*getTime(i)), ' ' * 10, end='\r', flush=True)
-		time.sleep(1)
-	notify('Pomodoro', '{} finished'.format(msg))
+    for i in range(0, seconds):
+        print(msg, formatTime(*getTime(i)), ' ' * 10, end='\r', flush=True)
+        time.sleep(1)
+    notify('Pomodoro', '{} finished'.format(msg))
 
 
 while True:
-	show_pomodoro()
-	show_break()
-
+    try:
+        show_pomodoro()
+        show_break()
+    except (KeyboardInterrupt):
+        sys.exit(0)
